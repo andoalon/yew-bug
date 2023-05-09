0d14a59d8fc2c2f8d3ae2bf9b732708aa00ceef5
andoalon <andoalon97@gmail.com>
2023-05-09 12:27:16 +0200

    Add commented-out code to easily switch to using yew 0.19.3

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod app;
 use app::App;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<App>::new().render(); // yew 0.20.0
+    //yew::start_app::<app::App>(); // yew 0.19.3
 }
